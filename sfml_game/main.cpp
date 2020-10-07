813648d1b15852a6343580059b2c4ce6d32a1a5b
CHURAIRAT DANGPHUNGPAIBOON <63010159@KMITL.AC.TH>
2020-10-08 03:07:42 +0700

    gravity

---
+++ sfml_game/main.cpp
@@ -0,0 +1,92 @@
+#include <SFML/Graphics.hpp>
+#include<iostream>
+#include<vector>
+#include"Player.h"
+#include"Platform.h"
+
+static const float View_HEIGHT = 720.0f;
+
+void ResizeView(const sf::RenderWindow& window, sf::View& view)
+{
+	float aspectRatio = float(window.getSize().x) / float(window.getSize().y);
+	view.setSize((View_HEIGHT)* aspectRatio, View_HEIGHT );
+}
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1080, 720), "Fila Kung");
+	sf::RectangleShape frame(sf::Vector2f(1080.0f, 1000.0f));
+	sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(View_HEIGHT, View_HEIGHT));
+
+
+	//sf::RectangleShape bg_yard1(sf::Vector2f(500,500);
+	//sf::Texture bg_yard;
+	//bg_yard.loadFromFile("img/bg_yard.jpg");
+	//bg_yard1.setTexture(&bg_yard);
+
+	sf::RectangleShape b0(sf::Vector2f(0,0));
+	sf::Texture floor;
+	floor.loadFromFile("img/floor.png");
+	b0.setTexture(&floor);
+	
+
+	sf::Texture mon_texture;
+	mon_texture.loadFromFile("img/cat2_3.png"); 
+	Player monster(&mon_texture, sf::Vector2u(3, 2), 0.3f, 100.0f,200.0f);
+	
+	std::vector<Platform> platforms;
+
+	//platforms.push_back(Platform(nullptr,sf::Vector2f(400.0f, 200.0f), sf::Vector2f(500.0f, 200.0f)));
+	//platforms.push_back(Platform(nullptr, sf::Vector2f(400.0f, 100.0f), sf::Vector2f(500.0f, 0.0f)));
+	platforms.push_back(Platform(&floor, sf::Vector2f(900.0f,100.0f), sf::Vector2f(300.0f, 500.0f)));
+	
+
+	float deltaTime = 0.0f;
+	sf::Clock clock;
+	while (window.isOpen())
+	{
+//		window.draw(bg_yard1);
+		window.draw(b0);
+		deltaTime = clock.restart().asSeconds();
+		if (deltaTime > 1.0f / 20.0f)
+			deltaTime = 1.0f / 20.0f;
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::Resized:
+					ResizeView(window, view);
+					break;
+			}
+		}
+		
+		monster.Update(deltaTime);
+		view.setCenter(monster.GetPosition());
+
+		Collider monsterCollision = monster.GetCollider(); // Dammit!!!!
+		sf::Vector2f direction;
+		
+
+		for (Platform& platform : platforms)
+			if (platform.GetCollider().CheckCollision(monsterCollision, direction, 1.0f))
+				monster.OnCollision(direction);
+		
+		
+		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+		window.clear(sf::Color(170, 237, 202));
+		window.setView(view);
+		monster.Draw(window);
+		for (Platform& platform : platforms)
+		platform.Draw(window);
+		window.display();
+	}
+	return 0;
+}
--- sfml_game/main101.cpp
+++ sfml_game/main101.cpp
@@ -6,10 +6,15 @@ int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1080, 720), "FILA-KUNG");
 
+	//const float gravity = 1;
+	//int groundHeight = 440;
+	//sf::Vector2f velocity(sf::Vector2f(0, 0));
+	//float jumpspeed = 10.0f;
+
 	////// Circle
-//	sf::CircleShape collision(100.f);
-//	collision.setPosition({ 200.f, 200.f });
-//	collision.setFillColor(sf::Color::Red);
+	sf::CircleShape collision(100.f);
+	collision.setPosition({ 400.f, 400.f });
+	collision.setFillColor(sf::Color::Blue);
 
 	////// Texture
 	sf::Texture playerTexture;
@@ -45,18 +50,27 @@ int main()
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 		{
 			shapeSprite.move(-.1f, 0.f);
-			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, spriteSizeY));
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 0, spriteSizeX, spriteSizeY));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 		{
 			shapeSprite.move(0.f, -.1f);
-//			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, 0, spriteSizeX, spriteSizeY));
+//			velocity.y = -jumpspeed;
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, 0, spriteSizeX, spriteSizeY));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 		{
 			shapeSprite.move(0.f, .1f);
-//			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, 32));
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, 32));
 		}
+		//if (shapeSprite.getPosition().y + spriteSizeY < groundHeight || velocity.y < 0)
+		//{
+		//	velocity.y += gravity;
+		//}
+		//else
+		//{
+		//	shapeSprite.setPosition(shapeSprite.getPosition().x, groundHeight - spriteSizeY);
+		//}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 		{
 			window.close();
@@ -70,7 +84,7 @@ int main()
 		if (collision.getGlobalBounds().intersects(shapeSprite.getGlobalBounds())) {
 			shapeSprite.setPosition(spawnPoint);
 		}
-		window.clear();
+		window.clear(sf::Color(0, 255, 127));
 	}
 	return 0;
 }
